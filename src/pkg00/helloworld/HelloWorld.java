aa8d0d432fbf20c5df2b50ce57daacb580bda11c
LouisLeBars <lebars.louis@proton.me>
2026-09-22 11:19:15 +0200

    oiusfehoûiqs<

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello World llb");
+        System.out.println("hello World lllb");
     }
     
 }
